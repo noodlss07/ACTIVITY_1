0d3b072fafa0ed751bcaacae0d188cdad4dcfd81
SULAR, LANCE ADRIAN R. <lancesular@gmail.com>
2026-02-14 17:56:27 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ACTIVITY_1_SULAR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("====ACCOUNT MANAGEMENT SYSTEM====");
+            string username = "sularlance";
+            string password = "sularlance123";
+
+            Console.Write("Enter username: ");
+            string inputUsername = Console.ReadLine();
+            Console.Write("Enter password: ");
+            string inputPassword = Console.ReadLine();
+
+            if (inputUsername == username && inputPassword == password)
+            {
+                Console.WriteLine("Login successful! Welcome, " + username + "!");
+            }
+            else
+            {
+                Console.WriteLine("Login failed! Invalid username or password.");
+            }   
+        }
+    }
+}
